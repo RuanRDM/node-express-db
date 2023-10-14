369b86eaf6972afaa9d895b7dbc5d18bc5171aec
RuanRDM <ruan.delatorre.rd@gmail.com>
2023-10-14 15:04:49 -0300

    add route editoras

--- routes/rotas.js
+++ routes/rotas.js
@@ -4,6 +4,8 @@ const { rotasCategorias } = require('./rotasCategorias');
 
 const { rotasProdutos} = require('./rotasProdutos');
 
+const { rotasEditoras} = require('./rotasEditoras');
+
 const { login } = require('../controllers/segurancaController');
 
 const rotas = new Router();
@@ -13,5 +15,6 @@ rotas.route('/login').post(login);
 
 rotas.use(rotasCategorias);
 rotas.use(rotasProdutos);
+rotas.use(rotasEditoras);
 
 module.exports = rotas;
