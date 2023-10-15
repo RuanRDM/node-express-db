d5ff57c070fb871d4b7ebc5e3c24ac0b8789f62d
RuanRDM <ruan.delatorre.rd@gmail.com>
2023-10-14 21:06:31 -0300

    remove old routes produto and categoria

--- routes/rotas.js
+++ routes/rotas.js
@@ -1,9 +1,5 @@
 const { Router } = require('express');
 
-const { rotasCategorias } = require('./rotasCategorias');
-
-const { rotasProdutos} = require('./rotasProdutos');
-
 const { rotasEditoras} = require('./rotasEditoras');
 
 const { rotasLivros} = require('./rotasLivros');
@@ -15,8 +11,6 @@ const rotas = new Router();
 // rota para o login
 rotas.route('/login').post(login);
 
-rotas.use(rotasCategorias);
-rotas.use(rotasProdutos);
 rotas.use(rotasEditoras);
 rotas.use(rotasLivros);
 
